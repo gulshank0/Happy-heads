de91084464a2a8e711d2c654d122c10dcb9444ff
gulshank0 <gulshanbahadur002@gmail.com>
2025-09-03 23:50:35 +0530

    project stuctured

---
+++ Backend/dist/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from "express";
+//import cors from "cors";
+const app = express();
+//app.use(cors());
+// Example API routes
+app.get("/api/ping", (req, res) => {
+    const ping = process.env.PING_MESSAGE ?? "ping";
+    res.json({ message: ping });
+});
+app.get("/api", (req, res) => {
+    res.json({ message: "API is running" });
+});
+app.get("/api/health", (req, res) => {
+    res.json({ status: "healthy" });
+});
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 8000}`);
+});
+//# sourceMappingURL=index.js.map
