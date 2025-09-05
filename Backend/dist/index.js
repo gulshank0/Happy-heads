5ddaa964a46bbd40f4e24d1d9408d8472c31ddaf
gulshank0 <gulshanbahadur002@gmail.com>
2025-09-05 14:19:16 +0530

    prisma init

--- Backend/dist/index.js
+++ Backend/dist/index.js
@@ -1,11 +1,17 @@
-import "dotenv/config";
-import express from "express";
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+require("dotenv/config");
+const express_1 = __importDefault(require("express"));
 //import cors from "cors";
-const app = express();
+const app = (0, express_1.default)();
 //app.use(cors());
 // Example API routes
 app.get("/api/ping", (req, res) => {
-    const ping = process.env.PING_MESSAGE ?? "ping";
+    var _a;
+    const ping = (_a = process.env.PING_MESSAGE) !== null && _a !== void 0 ? _a : "ping";
     res.json({ message: ping });
 });
 app.get("/api", (req, res) => {
@@ -17,4 +23,3 @@ app.get("/api/health", (req, res) => {
 app.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running on port ${process.env.PORT || 8000}`);
 });
-//# sourceMappingURL=index.js.map
